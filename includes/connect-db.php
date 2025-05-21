4ae2381aedb94994698aecf21ea2271f7171d2d3
sabcomp <alushisabrina93@gmail.com>
2025-05-21 18:54:01 +0200

    add customer registration

--- includes/connect-db.php
+++ includes/connect-db.php
@@ -4,8 +4,10 @@ $USERNAME = "root";
 $PASSWORD = "";
 $DB = "cinema";
 
-$conn = new mysqli($HOST, $USERNAME, $PASSWORD, $DB);
+@$conn = new mysqli($HOST, $USERNAME, $PASSWORD, $DB);
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    exit();
 }
+
+$salt = "73nf@%nud^ksQZ";
 ?>
--- register.php
+++ register.php
@@ -1,36 +1,90 @@
 <?php
+// Session management
+session_start();
+if (isset($_SESSION["email"]) && isset($_SESSION["password"]) && isset($_SESSION["role"])) {
+    if ($_SESSION["role"] === "manager") {
+        header("Location: manager.php");
+        exit();
+    } else if ($_SESSION["role"] === "customer") {
+        header("Location: index.php");
+        exit();
+    }
+}
+?>
+
+<?php
+// Adding customer to database
+$error = "";
+require_once("includes/connect-db.php");
+if (isset($_POST["submit"])) {
+    $name = $conn->real_escape_string($_POST["registerName"]);
+    $email = $conn->real_escape_string($_POST["registerEmail"]);
+    $password = $conn->real_escape_string($_POST["registerPassword"]);
+
+    // Hash the password
+    $password .= $salt;
+    $hashedPassword = hash("md5", $password);
+
+    if (!empty($name) && !empty($email) && !empty($password)) {
+        // Check if email already exists
+        $stmt = $conn->prepare("SELECT * FROM customers WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            $error = "Email already exists";
+        } else {
+            // Insert new manager into the database
+            $stmt = $conn->prepare("INSERT INTO customers(customer_name, email, password) VALUES (?, ?, ?)");
+            $stmt->bind_param("sss", $name, $email, $hashedPassword);
+            // Check if the account was created successfully
+            if ($stmt->execute()) {
+                header("Location: login.php");
+                exit();
+            } else {
+                $error = "Error creating account. Please try again.";  
+            }
+        }
+    }
+}
+
+?>
+<?php
 require("includes/header.php")
 ?>
 <main class="login-page">
     <div class="container">
         <div class="auth-container">
             <div class="auth-content">
-                <form id="register-form" class="auth-form active">
+                <form id="register-form" class="auth-form active" action="" method="POST">
                     <h2>Create an Account</h2>
                     <p>Join Star Cinema to book tickets, earn rewards, and more.</p>
 
                     <div class="form-group">
                         <label for="register-name">Full Name</label>
-                        <input type="text" id="register-name" required>
+                        <input type="text" id="register-name" name="registerName" required>
                     </div>
 
                     <div class="form-group">
                         <label for="register-email">Email</label>
-                        <input type="email" id="register-email" required>
+                        <input type="email" id="register-email" name="registerEmail" required>
                     </div>
 
                     <div class="form-group">
                         <label for="register-password">Password</label>
-                        <input type="password" id="register-password" required>
+                        <input type="password" id="register-password" name="registerPassword" required>
                     </div>
 
-                    <div class="form-group">
-                        <label for="register-confirm-password">Confirm Password</label>
-                        <input type="password" id="register-confirm-password" required>
-                    </div>
+                    <input type="submit" name="submit" value="Create Account" class="btn-primary">
                     
-                    <button type="submit" class="btn-primary">Create Account</button>
                 </form>
+                <?php
+                    if (!empty($error)) {?>
+                        <div class="form-group">
+                            <label style="color: red;"><?php echo $error;?></label>
+                        </div>
+                <?php } ?>
             </div>
         </div>
     </div>
