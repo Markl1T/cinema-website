b3ac8f261d6c85e0e368750b638c75793a3d82ec
Markli Tati <79922926+Markl1T@users.noreply.github.com>
2025-05-26 15:10:57 +0200

    fix manager.php

--- manager.php
+++ manager.php
@@ -6,18 +6,6 @@ displayHeader(true, false, false, true);
 <body>
     <div class="container">
         <h2 class="section-title">Movie Manager</h2>
-    <div class="container">
-        <h2 class="section-title">Movie Manager</h2>
-
-        <div class="manager-container">
-            <div class="manager-header">
-                <h3>Content Management</h3>
-                <div>
-                    <a href="add-movie.php" class="btn-primary">Add New Movie</a>
-                    <a href="add-screening.php" class="btn-primary">Add New Screening</a>
-                </div>
-
-            </div>
         <div class="manager-container">
             <div class="manager-header">
                 <h3>Content Management</h3>
@@ -28,10 +16,6 @@ displayHeader(true, false, false, true);
 
             </div>
 
-            <div class="manager-tabs">
-                <button class="manager-tab active" data-tab="movies">Movies</button>
-                <button class="manager-tab" data-tab="screenings">Screenings</button>
-            </div>
             <div class="manager-tabs">
                 <button class="manager-tab active" data-tab="movies">Movies</button>
                 <button class="manager-tab" data-tab="screenings">Screenings</button>
@@ -74,82 +58,7 @@ displayHeader(true, false, false, true);
                     </tbody>
                 </table>
             </div>
-            <div class="manager-content active" id="movies-content">
-                <table class="manager-table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Genre</th>
-                            <th>Duration</th>
-                            <th>Start Date</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        include("includes/connect-db.php");
-                        $movies = $conn->execute_query("SELECT * FROM movies ORDER BY start_date");
-                        if ($movies->num_rows > 0) {
-                            while ($movie = $movies->fetch_assoc()) {
-                                echo '<tr>';
-                                echo '<td>' . $movie['title'] . '</td>';
-                                echo '<td>' . $movie['genre'] .'</td>';
-                                echo '<td>'. $movie['duration'] .'</td>';
-                                echo '<td>'. date("F j", strtotime($movie['start_date'])) .'</td>';
-                                echo '<td>';
-                                echo '<div class="action-buttons">';
-                                echo '<button class="btn-edit">Edit</button>';
-                                echo '<button class="btn-delete">Delete</button>';
-                                echo '</div>';
-                                echo '</td>';
-                                echo '</tr>';
-                            }
-                        } else {
-                            echo "<p>No movies found.</p>";
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
 
-            <div class="manager-content" id="screenings-content">
-                <table class="manager-table">
-                    <thead>
-                        <tr>
-                            <th>Movie</th>
-                            <th>Date</th>
-                            <th>Time</th>
-                            <th>Theater</th>
-                            <th>Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        $screenings = $conn->execute_query("SELECT * FROM screenings s JOIN movies m ON s.movie_id = m.movie_id ORDER BY date, time");
-                        if ($screenings->num_rows > 0) {
-                            while ($screening = $screenings->fetch_assoc()) {
-                                echo '<tr>';
-                                echo '<td>' . $screening['title'] . '</td>';
-                                echo '<td>' . date("F j", strtotime($screening['date'])) .'</td>';
-                                echo '<td>'. date("H:i", strtotime($screening['time'])) .'</td>';
-                                echo '<td>'. $screening['theater_id'] .'</td>';
-                                echo '<td>';
-                                echo '<div class="action-buttons">';
-                                echo '<button class="btn-edit">Edit</button>';
-                                echo '<button class="btn-delete">Delete</button>';
-                                echo '</div>';
-                                echo '</td>';
-                                echo '</tr>';
-                            }
-                        } else {
-                            echo "<p>No screenings found.</p>";
-                        }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
             <div class="manager-content" id="screenings-content">
                 <table class="manager-table">
                     <thead>
